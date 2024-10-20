fb2802420cacd21b5bd94db81ddc4d1c4f14bd44
Vubeta <vubeta9@gmail.com>
2024-10-20 22:05:11 +0700

    up

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -1,33 +1,35 @@
 import React from "react";
+import './footer.css';
+import {ZaloIcon, ZaloIcon2} from "./FooterIcon";
 
 const Footer = () => {
     return (
         <div>
-            <div className="container">
+            <div className="container-fluid">
                 {/*Top Section*/}
                 <div className="row text-center py-3">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <h5>Gọi mua hàng (08:30-18:00)</h5>
                         <p><i className="fas fa-phone"></i> 0912241237</p>
                         <p>Tất cả các ngày trong tuần</p>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <h5>Góp ý, khiếu nại (08:30-20:30)</h5>
                         <p><i className="fas fa-phone"></i> 0912241237</p>
                         <p>Các ngày trong tuần (trừ ngày lễ)</p>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <h5>Đăng ký nhận thông tin mới</h5>
                         <form className="d-flex">
-                            <input type="email" className="form-control" placeholder="Nhập email của bạn" />
+                            <input type="email" className="form-control" placeholder="Nhập email của bạn"/>
                             <button type="submit" className="btn btn-dark">Đăng ký</button>
                         </form>
-                        <div className="mt-3">
-                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                            <a href="#"><i className="fab fa-instagram"></i></a>
-                            <a href="#"><i className="fab fa-youtube"></i></a>
-                            <a href="#"><i className="fab fa-zalo"></i></a>
-                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#" className="fb-icon"><i className="fab fa-facebook-f facebook"></i></a>
+                        <a href="#" className="ig-icon"><i className="fab fa-instagram instagram"></i></a>
+                        <a href="#" className="yt-icon"><i className="fab fa-youtube youtube"></i></a>
+                        <a href="#" className="zalo-icon"><ZaloIcon/></a>
                     </div>
                 </div>
 
@@ -59,16 +61,23 @@ const Footer = () => {
                     <div className="col-md-3">
                         <h5>Fanpage chúng tôi</h5>
                         {/*Fanpage content*/}
-                        <img src="fanpage-image.jpg" alt="Fanpage" className="img-fluid" />
+                        <img src="fanpage-image.jpg" alt="Fanpage" className="img-fluid"/>
                     </div>
                 </div>
             </div>
 
             {/*Social Media Buttons (Right Side)*/}
-            <div className="position-fixed" style={{ top: '50%', right: '10px' }}>
-                <a href="#" className="btn btn-danger mb-2"><i className="fas fa-phone"></i></a>
-                <a href="#" className="btn btn-primary mb-2"><i className="fab fa-facebook-messenger"></i></a>
-                <a href="#" className="btn btn-info"><i className="fab fa-zalo"></i></a>
+            <div className="position-fixed" style={{top: '50%', right: '10px'}}>
+                <a href="#" className="btn btn-danger mb-2 ft-icon"><i className="fas fa-phone"></i></a>
+                <a href="#" className="btn btn-primary mb-2 ft-icon"><i className="fab fa-facebook-messenger"></i></a>
+                <a href="#" className="btn btn-info ft-icon">
+                    <img
+                        src="https://web.nvnstatic.net/tp/T0298/img/zalo.svg?v=3"
+                        alt="Biểu tượng Zalo"
+                        width="50"
+                        height="50"
+                    />
+                </a>
             </div>
         </div>
     );
